d1c1e67a6368d630fd4637d95157ef8f716dd5af
armanisadeghi <132974515+armanisadeghi@users.noreply.github.com>
2024-05-21 19:03:31 -0700

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,32 @@
-import '@mantine/core/styles.css';
-import React from 'react';
-import { MantineProvider, ColorSchemeScript } from '@mantine/core';
-import { theme } from '../theme';
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
-export const metadata = {
-  title: 'AI Matrix',
-  description: 'Next-Generation AI Automation Framework!',
-};
+function Demo() {
+    const [opened, { toggle }] = useDisclosure();
 
-export default function RootLayout({ children }: { children: any }) {
-  return (
-    <html lang="en">
-      <head>
-        <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
-      </head>
-      <body>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
-      </body>
-    </html>
-  );
+    return (
+        <AppShell
+            header={{ height: 60 }}
+            navbar={{
+                width: 300,
+                breakpoint: 'sm',
+                collapsed: { mobile: !opened },
+            }}
+            padding="md"
+        >
+            <AppShell.Header>
+                <Burger
+                    opened={opened}
+                    onClick={toggle}
+                    hiddenFrom="sm"
+                    size="sm"
+                />
+                <div>Logo</div>
+            </AppShell.Header>
+
+            <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+            <AppShell.Main>Main</AppShell.Main>
+        </AppShell>
+    );
 }
